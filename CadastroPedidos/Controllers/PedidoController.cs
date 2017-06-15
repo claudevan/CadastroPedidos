ce5bd93fdbfbbee650421f108cd9a60e451611d3
Claudevan José da Silva <claudevan.ti@gmail.com>
2017-06-15 16:21:43 -0300

    Add fontawesome

---
+++ CadastroPedidos/Controllers/PedidoController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CadastroPedidos.Controllers
+{
+    public class PedidoController : Controller
+    {
+        // GET: Pedido
+        public ActionResult Index()
+        {
+            
+            List<SelectListItem> clientes = new List<SelectListItem>();
+            clientes.Add(new SelectListItem() { Text = "Cliente 1", Value = "1" });
+            clientes.Add(new SelectListItem() { Text = "Cliente 2", Value = "2" });
+            clientes.Add(new SelectListItem() { Text = "Cliente 3", Value = "3" });
+            clientes.Add(new SelectListItem() { Text = "Cliente 4", Value = "4" });
+
+            ViewBag.Clientes = clientes;
+
+            return View();
+        }
+    }
+}
