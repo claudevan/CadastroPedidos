c333aced7bb97d42ee4b1140eb3b1ad9587dccd5
Claudevan José da Silva <claudevan.ti@gmail.com>
2017-06-18 23:12:04 -0300

    Criação de Home com apresentação do projeto

--- CadastroPedidos/App_Start/RouteConfig.cs
+++ CadastroPedidos/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace CadastroPedidos
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Pedido", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
